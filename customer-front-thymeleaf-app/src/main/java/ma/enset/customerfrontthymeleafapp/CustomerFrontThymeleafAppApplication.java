d24963dd030dbf8913d7c0ab13f74f9c651b2dc0
user <user@192.168.11.104>
2024-01-27 16:50:55 +0100

    all project

---
+++ customer-front-thymeleaf-app/src/main/java/ma/enset/customerfrontthymeleafapp/CustomerFrontThymeleafAppApplication.java
@@ -0,0 +1,25 @@
+package ma.enset.customerfrontthymeleafapp;
+
+import ma.enset.customerfrontthymeleafapp.entities.Customer;
+import ma.enset.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerFrontThymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFrontThymeleafAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().name("othmane").email("othmane@gmail.com").build());
+            customerRepository.save(Customer.builder().name("darhoni").email("darhoni@gmail.com").build());
+            customerRepository.save(Customer.builder().name("kapera").email("kapera@gmail.com").build());
+        };
+    }
+}
